92be69a64a257760da57dc960b9fdfbd7ed174ab
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-03-01 09:45:48 +0800

    init: initialize my astro_init sequence
    i don't know where its going but i figured i should just write in bash, i
    mean what's the point in making it all fancy right?

---
+++ README.md
@@ -0,0 +1,27 @@
+# astro_init
+
+Is just an initialization script for Astro.
+It will scaffold a base project
+
+## dependencies
+
+use the `ASTRO_INIT__SOURCE_ROOT_PATH` to specify the
+where it's going to copy the files from
+
+i just symlink the sh file to my `~/bin`, and then do:
+
+```sh
+env ASTRO_INIT__SOURCE_ROOT_PATH=path/to/dir astro_init.sh
+```
+
+it does not have a default, it should just error if
+it can't find the path dir
+
+oh, and it also requires `tmux` to run the dev server in
+and `bun` for the runtime
+
+## road map
+
+i need to figure out how to get it so the actual astro
+create script from npm to stop asking questions and just
+do the thing,
---
+++ astro_init.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/bash
+
+let es=0
+
+catch() {
+    if [[ $es == 0 ]]; then
+        {
+            echo "did not expect an error but got one"
+            return
+        }
+    fi
+    echo "astro_init.sh: error: $es"
+    exit 1
+}
+
+try() {
+    set -e
+    "$@"
+}
+
+main() {
+    echo "thank you for using astro_init.sh"
+
+    es=$es+1
+    bun create astro@latest -- --template minimal . &&
+        echo "done installing astro"
+
+    es=$es+1
+    bun install --save-dev \
+        eslint eslint-plugin-astro \
+        @typescript-eslint/parser \
+        prettier \
+        prettier-plugin-astro \
+        eslint-plugin-jsx-a11y \
+        jiti &&
+        echo "done installing basic packages"
+
+    es=$es+1
+    bunx astro add astro-compressor &&
+        echo "added integration package"
+
+    es=$es+1
+    rm -r ./src ./public &&
+        cp -r \
+            $ASTRO_INIT__SOURCE_ROOT_PATH/src \
+            $ASTRO_INIT__SOURCE_ROOT_PATH/public \
+            $ASTRO_INIT__SOURCE_ROOT_PATH/eslint.config.ts \
+            $ASTRO_INIT__SOURCE_ROOT_PATH/prettier.config.mjs \
+            . &&
+        echo "moved some stuff"
+
+    es=$es+1
+    sed -i -E 's|("extends": ".*)strict(".*)|\1strictest\2|' ./tsconfig.json &&
+        sed -i 's/favicon\.svg/sword\.svg/g' ./src/layouts/Layout.astro &&
+        echo "changed tsconfig"
+
+    es=$es+1
+    rm README.md &&
+        echo "deleted readme"
+
+}
+
+try main || catch &&
+    if ! tmux ls | grep -q "astro_dev"; then
+        {
+            tmux new-session -d -s astro_dev "bun run dev"
+            exit 0
+        }
+    fi
+tmux kill-session -t astro_dev &&
+    tmux new-session -d -s astro_dev "bun run dev"
